908907212260d2aeaaed01a8b5388300ff4d62a9
munirapp <softwaremakassar@gmail.com>
2023-08-09 11:13:30 +0800

    feat: add validate request body middleware

--- src/backend/middleware/__init__.py
+++ src/backend/middleware/__init__.py
@@ -1,8 +1,9 @@
 from functools import wraps as __wraps
 from flask import request as __request, jsonify as __jsonify
+from jsonschema import validate as __validate, ValidationError as __ValidationError
 
 
-def __requires_authorization(f):
+def requires_authorization(f):
     @__wraps(f)
     def decorated(*args, **kwargs):
         try:
@@ -18,4 +19,18 @@ def __requires_authorization(f):
 
     return decorated
 
-requires_authorization = __requires_authorization
+def validate_request_body(schema_json):
+    def decorator(f):
+        @__wraps(f)
+        def decorated(*args, **kwargs):
+            try:    
+                if schema_json is None:
+                    raise __ValidationError('schema json is empty')
+                __validate(instance=__request.get_json(),schema=schema_json)
+                return f(*args, **kwargs)
+            except __ValidationError as validation_error:
+                return __jsonify({"errorMessage": str(validation_error)}), 400
+            except Exception as e:
+                return __jsonify({"errorMessage": str(e)}), 500
+        return decorated
+    return decorator
