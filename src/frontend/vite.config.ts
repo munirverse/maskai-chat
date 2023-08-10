df2ba2a5a94a944b29d34d1afb5f29e407509e5b
munirapp <softwaremakassar@gmail.com>
2023-08-10 09:32:31 +0800

    chore: add env load process in vite config files

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import { config as dotenvConfig } from 'dotenv';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default () => {
+    const envDir = path.join(process.cwd(), '../../.env');
+    dotenvConfig({ path: envDir });
+
+    // https://vitejs.dev/config/
+    return defineConfig({
+        plugins: [react()],
+        server: { port: parseInt(process.env.FE_PORT) },
+    });
+};
