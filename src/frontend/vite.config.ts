4c0af5b165bf31872cd1a3b5e8656218c81721bd
munirapp <softwaremakassar@gmail.com>
2023-09-06 23:01:47 +0800

    chore: improve env load logic

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,10 +1,13 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import { config as dotenvConfig } from 'dotenv';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default () => {
-    const envDir = path.join(process.cwd(), '../../.env');
+    let envPath = '.env';
+    if (!fs.existsSync('.env')) envPath = '../../.env';
+    const envDir = path.join(process.cwd(), envPath);
     dotenvConfig({ path: envDir });
 
     // https://vitejs.dev/config/
